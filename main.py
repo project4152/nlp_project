553bcaece8ef4964b66d3197366966d79bd2b366
xiangling1994 <xn947387@dal.ca>
2017-03-28 00:13:51 -0300

    Revert "add user generate in main"
    This reverts commit 71e80ca9430a9d9994fc3380ce168413524e5b34.

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from preprocess import  *
 from svm import *
 from svm import svm_data_transform
-from preprocess import generate_tweet_user
 
 def main(file):
-
     svm_data_transform.train_svm_model(file)
-    # for output it will print the user id, user's name, user's decription, user's loction, and user's tweets
-    users = generate_tweet_user.generatetweetuser("datacollection/tweets_json.txt")
-    print users[0].__str__()
 
 if __name__ == "__main__":
     main("Data.txt")
