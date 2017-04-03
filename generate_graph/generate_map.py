9d0ab47ec25a1ab48f54c510bfedc131cc4c5d57
Seanliuxuhui <Seanliuxuhui@gmail.com>
2017-04-03 18:25:07 -0300

    add Canada Happiness json file

--- generate_graph/generate_map.py
+++ generate_graph/generate_map.py
@@ -25,11 +25,39 @@ def draw_map(happiness_index_map):
     # province_df = pd.DataFrame({'NAME': province_names}, dtype=str)
     #
     #translate province name into province code
-    province_code_happiness_index_map = dict()
-    for (province, happiness) in happiness_index_map.items():
-        province_code_happiness_index_map[canada_abbr.abbr_dict().get(province)] = happiness
+    # province_code_happiness_index_map = dict()
+    # for (province, happiness) in happiness_index_map.items():
+    #     province_code_happiness_index_map[canada_abbr.abbr_dict().get(province)] = happiness
 
-    province_code_happiness_df = pd.DataFrame({'NAME': province_code_happiness_index_map.keys(), "Happiness": province_code_happiness_index_map.values()})
-    pprint.pprint(province_code_happiness_df)
+    province_happiness_df = pd.DataFrame({'Province Name': happiness_index_map.keys(), "Happiness": happiness_index_map.values()})
+    province_province_code_df = pd.DataFrame({"Province Name": canada_abbr.abbr_dict().keys(), "NAME": canada_abbr.abbr_dict().values()}, dtype=str)
+    province_province_code_happiness_df = pd.merge(province_happiness_df, province_province_code_df, on="Province Name", how='inner')
+
+    province_province_code_happiness_df = province_province_code_happiness_df.fillna(method='pad')
+    province_topo = r'generate_graph/canada.topo.json'
+
+    geo_data = [
+        {
+            'name': 'provinces',
+            'url': province_topo,
+            'feature': 'collection'
+        }
+    ]
+
+    #create map of canada, color coded based upon on the positive happiness index
+    vis = vince.Map(data=province_province_code_happiness_df,
+                    geo_data=geo_data,
+                    scale=1100,
+                    projection='albersUsa',
+                    data_bind='Happiness',
+                    data_key='NAME',
+                    map_key={
+                        'provinces': 'properties.NAME'
+                    })
+    vis.marks[0].properties.enter.stroke_opacity = vince.ValueRef(value=0.5)
+    vis.scales['color'].type= 'threshold'
+
+    vis.legend(title='Hapiness by Provinces')
+    vis.to_json('Canada Happiness.json')
 
 
